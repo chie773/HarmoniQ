58512bdd983b9bfcf287c665b0840e4f524ef6b9
Chibueze Eburuoh <ceburuoh@gmail.com>
2025-12-12 06:54:23 -0500

    Reworked Various Commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,10 @@
 import { Message } from 'discord.js';
-import { handleJoin, handleLeave } from './voice.ts';
-import { MusicCommands } from './music.ts';
-import { AudioPlayerManager } from '../audio/player.ts';
-import { Queue } from '../audio/queue.ts';
+import { handleJoin, handleLeave } from './voice.js';
+import { MusicCommands } from './music.js';
+import { AudioPlayerManager } from '../audio/player.js';
+import { Queue } from '../audio/queue.js';
+
+
 
 export class CommandRouter {
     private musicCommands: MusicCommands;
@@ -25,43 +27,64 @@ export class CommandRouter {
             case '!join':
                 handleJoin(msg);
                 break;
-
             case '!leave':
                 handleLeave(msg);
                 break;
-
             case '!echo':
                 this.handleEcho(msg, args);
                 break;
-
             case 'hello':
                 this.handleHello(msg);
                 break;
-
+            case '!help':
+                break;
+            case '!add':
             case '!play':
                 await this.musicCommands.handlePlay(msg, args);
                 break;
-
             case '!pause':
                 this.musicCommands.handlePause(msg);
                 break;
-
             case '!unpause':
                 this.musicCommands.handleUnpause(msg);
                 break;
-
             case '!loop':
                 this.musicCommands.handleLoop(msg);
                 break;
-
             case '!unloop':
                 this.musicCommands.handleStopLoop(msg);
                 break;
-
             case '!skip':
                 this.musicCommands.handleSkip(msg);
                 break;
+            case '!q':
+            case '!queue':
+                this.musicCommands.handleViewQueue(msg);
+                break;
+            case '!duration':
+                this.musicCommands.handleSongDuration(msg);
+                break;
+            case '!shuffle':
+                this.musicCommands.handleShuffleQueue(msg);
+                break;
+            case '!clear':
+                this.musicCommands.handleClearQueue(msg);
+                break;
+            case '!remove':
+                this.musicCommands.handleRemoveFromQueue(msg, args);
+                break;
+            case '!skipTo':
+                this.musicCommands.handleSkipTo(msg, args[0]);
+                break;
+            case '!current':
+                this.musicCommands.handleCurrentSong(msg);
+                break;
+                
+            case '!shift':
+                break;
 
+            case '!seek':
+                this.musicCommands.handleSeek(msg, args);
             default:
             // ignore
         }
